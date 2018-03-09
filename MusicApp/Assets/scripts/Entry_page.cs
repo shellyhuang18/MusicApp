8ce004dae4af3ecd2d0180d9c0aded35f4489000
sacitgonen <sacit.gonen41@myhunter.cuny.edu>
2018-03-09 16:12:11 -0500

    initiated sign in and registration and firebase authentication

---
+++ MusicApp/Assets/scripts/Entry_page.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine;
+
+public class Entry_page : MonoBehaviour {
+
+	public InputField email;
+	public InputField password;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void login_submit (string sceneName) {
+		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync (email.text, password.text).ContinueWith((obj) => {
+			SceneManager.LoadScene (sceneName);
+		});
+	}
+
+	public void register_page (string sceneName) {
+		SceneManager.LoadScene (sceneName);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ MusicApp/Assets/scripts/Registration_Page.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine;
+
+public class Registration_Page : MonoBehaviour {
+
+	public InputField email;
+	public InputField password;
+	public InputField fname;
+	public InputField lname;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+	public void register_submit (string sceneName) {
+		FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(email.text, password.text).ContinueWith((obj) => {
+			SceneManager.LoadSceneAsync (sceneName);
+			});
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
